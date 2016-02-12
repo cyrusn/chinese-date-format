efae219ddab15cc102e50d5714ed6291f991b200
cyrusn <cyrusncy@gmail.com>
2016-02-13 00:55:07 +0800

    first commit
     Changes to be committed:
    	new file:   .gitignore
    	new file:   README.md
    	new file:   index.js
    	new file:   package.json

---
+++ README.md
@@ -0,0 +1,1 @@
+# Chinese Date Format
---
+++ index.js
@@ -0,0 +1,44 @@
+const CONSTANT = ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九', '十', '十一', '十二'];
+const TEN_DIGIT = '十';
+
+function yearFormat (year) {
+  const result = [];
+
+  year.toString().split('').forEach(letter => {
+    result.push(CONSTANT[parseInt(letter, 10)]);
+  });
+
+  result.push('年');
+  return result.join('');
+}
+
+function monthFormat (month) {
+  return CONSTANT[month + 1] + '月';
+}
+
+function dayFormat (day) {
+  const tenDigit = Math.floor(day / 10);
+  const result = [];
+  const firstDigit = day % 10;
+
+  if (tenDigit > 0) {
+    result.push((tenDigit > 1 ? CONSTANT[tenDigit] : '') + TEN_DIGIT);
+  }
+
+  if (firstDigit !== 0) {
+    result.push(CONSTANT[firstDigit]);
+  }
+
+  return result.join('') + '日';
+}
+
+function dayOfWeekFormat (dayOfWeek) {
+  if (dayOfWeek === 0) {
+    return '星期日';
+  }
+  return '星期' + CONSTANT[dayOfWeek];
+}
+
+module.exports = function (date) {
+  return yearFormat(date.getFullYear()) + monthFormat(date.getMonth()) + dayFormat(date.getDate()) + dayOfWeekFormat(date.getDay());
+};
