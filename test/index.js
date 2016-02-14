1b71737a9cd187941fa00ddf529d9cb746077b80
leesei <leesei@gmail.com>
2016-02-14 23:10:51 +0800

    use istanbul for coverage, improve test coverage
     Changes to be committed:
    	modified:   package.json
    	modified:   test/index.js

--- test/index.js
+++ test/index.js
@@ -2,8 +2,6 @@ const test = require('tape');
 const Formatter = require('..');
 
 test('with string', function (t) {
-  t.plan(6);
-
   // double digit month and day, Sunday
   const chtDate = new Formatter('2014-10-19');
   t.equal(chtDate.full, '二零一四年十月十九日');
@@ -12,32 +10,42 @@ test('with string', function (t) {
   t.equal(chtDate.month, '十月');
   t.equal(chtDate.day, '十九日');
   t.equal(chtDate.dayOfWeek, '星期日');
+
+  t.end();
 });
 
 test('with Date', function (t) {
-  t.plan(4);
-
   // single digit month and day, with Date object
   const chtDate = new Formatter(new Date('1776-7-4'));
   t.equal(chtDate.full, '一七七六年七月四日');
   t.equal(chtDate.month, '七月');
   t.equal(chtDate.day, '四日');
   t.equal(chtDate.dayOfWeek, '星期四');
+
+  t.end();
+});
+
+test('with Date', function (t) {
+  new Formatter(); // eslint-disable-line
+  // cannot assert on Date.now()
+  t.end();
 });
 
 test('months', function (t) {
-  t.plan(3);
   t.equal((new Formatter('2014-9-1')).month, '九月');
   t.equal((new Formatter('2014-10-1')).month, '十月');
   t.equal((new Formatter('2014-11-1')).month, '十一月');
+
+  t.end();
 });
 
 test('days', function (t) {
-  t.plan(6);
   t.equal((new Formatter('2014-10-9')).day, '九日');
   t.equal((new Formatter('2014-10-19')).day, '十九日');
   t.equal((new Formatter('2014-10-29')).day, '二十九日');
   t.equal((new Formatter('2014-10-10')).day, '十日');
   t.equal((new Formatter('2014-10-20')).day, '二十日');
   t.equal((new Formatter('2014-10-30')).day, '三十日');
+
+  t.end();
 });
